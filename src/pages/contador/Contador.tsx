8262d85f452842b154512e8a59221f73c94ee0f4
alexaniatoma <alexaniatoma@hotmail.com>
2025-04-04 11:53:13 -0300

    props e hook

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,22 @@
+﻿import { useState } from "react";
+
+function Contador() {
+  // let num = 0; -> não funciona
+
+  const[num, setNumero] = useState(0);
+
+  function somar() {
+    setNumero(num + 1);
+   
+  }
+
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+      <p>O numero atual é: {num}</p>
+      <button onClick={somar}>Aumentar +1 </button>
+    </div>
+  )
+}
+
+export default Contador
